f71948d2f24fc72ef7d3f250576e7f4730e4c64d
felipesvm <federt49@gmail.com>
2024-11-14 17:08:05 -0300

    Departments Class created

---
+++ SalesWebMvc/Controllers/DepartmentsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
+
+namespace SalesWebMvc.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Department> departments = new List<Department>();
+            return View();
+        }
+    }
+}
